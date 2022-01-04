e1b62ef119cae349e5019dab2caab7132e22794c
ntqams30 <msievers@thegeneral.com>
2022-01-04 08:30:07 -0600

    7.2 Eureka Server

---
+++ src/test/java/com/gcu/topic72es/Topic72esApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu.topic72es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Topic72esApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
